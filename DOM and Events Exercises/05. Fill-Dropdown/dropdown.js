8e2b63b0610d2db89567b50cb39810e9aaa17bbc
ganukov <ganukoff@gmail.com>
2023-03-23 15:36:51 +0000

    more exercises

--- DOM and Events Exercises/05. Fill-Dropdown/dropdown.js
+++ DOM and Events Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+    const newItemText = document.getElementById('newItemText');
+    const newItemValue = document.getElementById('newItemValue');
+    const selectMenu = document.getElementById('menu');
+
+    let newOption = document.createElement('option');
+    newOption.value = newItemValue.value;
+    newOption.textContent = newItemText.value;
+    selectMenu.appendChild(newOption);
+    newItemText.value = '';
+    newItemValue.value = '';
 }
--- DOM and Events Exercises/06. Table - Search Engine/solution.js
+++ DOM and Events Exercises/06. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
+   const searchInput = document.getElementById('searchField');
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const allRows = Array.from(document.querySelectorAll('tbody tr'));
+      const searchedWord = searchInput.value;
+
+      for (const row of allRows) {
+         let trimmedText = row.textContent.trim();
+         if (row.classList.contains('select')){
+            row.classList.remove('select');
+         }
+
+         if (trimmedText.includes(searchedWord)){
+            row.classList.add('select')
+         }
+      }
+      searchInput.value = ''
 
    }
 }
