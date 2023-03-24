9ccaa355972e689df0cad83b18706cb1989ae336
ganukov <ganukoff@gmail.com>
2023-03-24 12:40:37 +0000

    DOM exercises

--- DOM and Events Exercises/07. Format the Text/solution.js
+++ DOM and Events Exercises/07. Format the Text/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO
+  const inputText = Array.from(document.getElementById('input').value.split('. '));
+  const outputText = document.getElementById('output');
+  console.log(inputText.length)
+  console.log(inputText)
+  if (inputText.length > 1 && inputText.length <= 3) {
+    const newParagraph = outputText.appendChild(document.createElement('p'));
+    newParagraph.textContent = `${inputText.join('. ')}.`;
+  } else {
+    let numberOfParagraph = Math.floor(inputText.length / 3);
+    for (let i = 0; i < numberOfParagraph; i++) {
+      console.log(numberOfParagraph);
+      let eachParagraph = [];
+      while (eachParagraph.length < 3) {
+        let sentence = inputText.shift();
+        eachParagraph.push(sentence);
+      }
+      const newParagraph = outputText.appendChild(document.createElement('p'));
+      newParagraph.textContent = `${eachParagraph.join('.')}.`;
+    }
+    if (inputText.length > 0) {
+      let lastText = inputText;
+      const newParagraph = outputText.appendChild(document.createElement('p'));
+      newParagraph.textContent = lastText.join('.');
+    }
+  }
 }
+
+
