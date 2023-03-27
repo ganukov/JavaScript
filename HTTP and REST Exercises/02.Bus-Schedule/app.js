bf14b7e23fb8df7f4cda554a1311e7a49f34f26e
ganukov <ganukoff@gmail.com>
2023-03-28 00:31:35 +0100

    solved Bus-Schedule

--- HTTP and REST Exercises/02.Bus-Schedule/app.js
+++ HTTP and REST Exercises/02.Bus-Schedule/app.js
@@ -1,10 +1,43 @@
 function solve() {
     function depart() {
-        // TODO: 
+        const departBtn = document.getElementById('depart');
+        const arriveBtn = document.getElementById('arrive');
+        const infoBox = document.getElementsByClassName('info')[0];
+        let currentStop = 'depot'
+        const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+        fetch(`${BASE_URL}${currentStop}`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                infoBox.textContent = `Next stop ${data.name}`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+                currentStop = data.next
+            })
+            .catch((err) => {
+                infoBox.textContent = 'Error';
+                arriveBtn.disabled = true;
+                departBtn.disabled = true;
+            })
     }
 
     async function arrive() {
-        // TODO:
+        const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+        const departBtn = document.getElementById('depart');
+        const arriveBtn = document.getElementById('arrive');
+        const infoBox = document.getElementsByClassName('info')[0];
+        try {
+            const firstStop = await fetch(`${BASE_URL}depot`);
+            const data = await firstStop.json();
+            infoBox.textContent = `Arriving at ${data.name}`;
+            departBtn.disabled = false;
+            arriveBtn.disabled = true;
+        }
+        catch (err) {
+            infoBox.textContent = err.message;
+            arriveBtn.disabled = true;
+            departBtn.disabled = true;
+        }
     }
 
     return {
