cf9bc898ea177a1374653da000d73f06140350c1
ganukov <ganukoff@gmail.com>
2023-03-28 15:40:44 +0100

    solved Messenger

--- HTTP and REST Exercises/05.Messenger/app.js
+++ HTTP and REST Exercises/05.Messenger/app.js
@@ -1,5 +1,44 @@
 function attachEvents() {
-    // TODO:
-}
+    const author = document.querySelector("input[name='author']");
+    const content = document.querySelector("input[name='content']")
+    const textArea = document.getElementById('messages');
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+    const BASE_URL = 'http://localhost:3030/jsonstore/messenger';
 
+    submitBtn.addEventListener('click', submit);
+    refreshBtn.addEventListener('click', refresh)
+
+
+    function submit() {
+        let message = { author: author.value, content: content.value }
+
+        fetch(`${BASE_URL}`, {
+            method: "POST", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(message),
+        });
+        textArea.value += `${author.value}: ${content.value}\n`;
+        author.value = "";
+        content.value = "";
+    }
+
+    function refresh() {
+        fetch(`${BASE_URL}`)
+            .then((res) => res.json())
+            .then((data) => {
+                for (const each in data) {
+                    let wholeMessage = data[each];
+                    if (wholeMessage.author !== 'George') {
+                        textArea.textContent += `${wholeMessage.author}: ${wholeMessage.content}\n`;
+                    } else {
+                        textArea.textContent += `${wholeMessage.author}: ${wholeMessage.content}`;
+                    }
+                }
+            })
+    }
+
+}
 attachEvents();
